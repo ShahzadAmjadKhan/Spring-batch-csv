5ae1ff53f78ed27d4f09e67d07f2a40ac717f7d4
Shahzad Amjad Khan <157458532+ShahzadAmjadKhan@users.noreply.github.com>
2024-01-30 10:22:59 +0500

    Add files via upload

--- src/test/java/io/github/lscsv/ApplicationTest.java
+++ src/test/java/io/github/lscsv/ApplicationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -19,5 +20,13 @@ public class ApplicationTest {
     public void testGetIndexHtml() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/index.html"))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+    // @Test
+    public void testLscsvBatch() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/lscv")
+                .param("file", "/tmp/custom_2017_2020.csv"))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
     }
 }
