6034b200d8f9d5fd5111d4f38a5fc4574decf39c
Shahzad Amjad Khan <157458532+ShahzadAmjadKhan@users.noreply.github.com>
2024-01-31 10:16:04 +0500

    Update LogMessageSanitizer.java

--- src/main/java/io/github/lscsv/util/LogMessageSanitizer.java
+++ src/main/java/io/github/lscsv/util/LogMessageSanitizer.java
@@ -1,10 +1,18 @@
 package io.github.lscsv.util;
 
+import java.util.regex.Pattern;
+
 public final class LogMessageSanitizer {
 
+    private static final Pattern SANITIZER = Pattern.compile("[\r\n]");
+    
     public static String sanitize(String value) {
         return value == null ? "" : value.replaceAll("\n", "").
                 replaceAll("\r","").
                 replaceAll("\\R","");
+    }
+
+    public static String sanitizeWithPattern(String value) {
+        return value == null ? "" : SANITIZER.matcher(value).replaceAll("");
     }
 }
